c21eff61a9b6a714da7ad4dd96793a0e650f8ec8
jeanneandlydia <bbmmall@yahoo.com>
2018-05-31 15:49:00 +0000

    commit 345

--- cdemo/userinput.c
+++ cdemo/userinput.c
@@ -9,16 +9,24 @@ float areaOfCircle (float radius)
 	return area;
 }
  int main()
+
 {
 	char input [256];
 	float radiusmin, radiusmax;
 	printf("Enter the minimum value for the range of the radius: \n");
 	fgets(input, 256, stdin);
 	sscanf (input, "%f", &radiusmin);
+
 	printf("Enter the maximum value for the range of the radius: \n");
 	fgets(input, 256, stdin);
 	sscanf(input, "%f", &radiusmax);
 	printf("The area of the circle will range from %f to %f.\n", areaOfCircle(radiusmin), areaOfCircle(radiusmax));
+
+	printf("Possible values for area of circle in that range include: \n");
+	for (float i = radiusmin; i <= radiusmax; i++)
+	{
+	areaOfCircle (i);
+	}
 }
 
 
