e600c62be0d6bb5324a0b50873905e6758ee49cf
jeanneandlydia <bbmmall@yahoo.com>
2018-06-11 22:08:57 +0000

    newest update

--- cdemo/myproject/student.c
+++ cdemo/myproject/student.c
@@ -2,17 +2,17 @@
 #include <string.h>
 
 struct Student {
-	char firstname[30];
-	char lastname[30];
-	int age;
-	int studentid;
+        char firstname[30];
+        char lastname[30];
+        int age;
+        int studentid;
 };
 
 void printStudent(struct Student* student)
 {
-	printf("The following are the details for student %s %s.\n", student->firstname, student->lastname);
-	printf("age: %d\n",student->age);
-	printf("student ID: %d\n", student->studentid);
+        print("The following are the details for student %s %s.\n", student->f$
+        printf("age: %d\n",student->age);
+        printf("student ID: %d\n", student->studentid);
 }
 
 int main()
